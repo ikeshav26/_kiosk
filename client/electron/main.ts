4744016704d557839efab221eb257205334c1c90
ikeshav26 <keshavgilhotra4@gmail.com>
2026-03-17 23:09:21 +0530

    Fixed build error

--- client/electron/main.ts
+++ client/electron/main.ts
@@ -48,6 +48,7 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
       contextIsolation: false,
+      webSecurity: false,
     },
   });
 
